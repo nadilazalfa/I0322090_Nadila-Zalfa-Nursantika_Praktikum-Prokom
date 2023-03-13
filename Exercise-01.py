c8f2e2220507fd87728e1a8fa1cfbb398649d83a
nadilazalfa <nadilazalfaa@student.ac.id>
2023-03-13 23:55:56 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,14 @@
+nama = "Nadila Zalfa Nursantika"
+nim = "I0322090"
+tempat_tinggal = "Semarang"
+visualisasi = "ibukota Jawa Tengah yang terletak di sebelah utara pulau Jawa Tengah"
+harapan = "adalah saya bisa menjadi seorang pengusaha yang sukses yang memegang teguh agama"
+print ()
+
+print("Nama Saya", nama)
+print("nim", nim)
+print('')
+print("Saya berasal dari", tempat_tinggal)
+print("Semarang merupakan", visualisasi)
+print("")
+print("Harapan saya setelah lulus dari Teknik Industri", harapan)
